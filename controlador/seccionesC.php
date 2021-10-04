9a4046afc9d9b8e3a1666a14bb350f8228aebaf0
AMendoza180890 <allanmaleman@gmail.com>
2021-10-03 20:41:46 -0600

    se normalizo los controladores, modelos y los formularios de las secciones

--- controlador/seccionesC.php
+++ controlador/seccionesC.php
@@ -7,7 +7,7 @@ class seccionclassC{
             foreach ($obtenerSecciones as $key => $value) {
                 echo '<div class="grid-item">
                             <img class="img-responsive" alt="" src="Admin/'.$value["imagen"].'">
-                            <a href="'.$value["enlace"].'" class="project-description">
+                            <a href="'.$value["enlace"].'" target="_blank" class="project-description">
                                 <div class="project-text-holder">
                                 <div class="project-text-inner">
                                     <h3>'.$value["descripcionCorta"]. '</h3>
--- vista/modulos/encabezado.php
+++ vista/modulos/encabezado.php
@@ -5,17 +5,17 @@
   <meta charset="UTF-8">
   <meta content="IE=edge" http-equiv="X-UA-Compatible">
   <meta content="width=device-width,initial-scale=1" name="viewport">
-  <meta content="Page description" name="description">
+  <meta name="description" content="Tesoros de Dios, God’s Treasures, is a non-profit organization that serves children with disabilities in Nicaragua, providing a variety of services to the children and their families.">
   <meta name="google" content="notranslate" />
-  <meta content="Mashup templates have been developped by Orson.io team" name="author">
+  <meta content="tesorosdedios.org" name="author">
 
   <!-- Disable tap highlight on IE -->
   <meta name="msapplication-tap-highlight" content="no">
-  
+
   <link href="vista/assets/apple-icon-180x180.png" rel="apple-touch-icon">
   <link href="vista/assets/favicon.ico" rel="icon">
 
-  <title>Title page</title>  
+  <title>Tesoros de Dios Campaign</title>
 
   <link href="vista/css/main.82cfd66e.css" rel="stylesheet">
 </head>
