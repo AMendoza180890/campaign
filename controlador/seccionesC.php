45a292c7484ccc9dd50bc801ca26b5dab2fd84f0
Allan Jose Mendoza Aleman <allanmaleman@gmail.com>
2021-10-25 18:56:02 -0600

    Se realizó cambio en la vista de la página web y se realizó respaldo de la base de datos

--- controlador/seccionesC.php
+++ controlador/seccionesC.php
@@ -7,13 +7,14 @@ class seccionclassC{
             foreach ($obtenerSecciones as $key => $value) {
                 echo '<div class="grid-item">
                             <img class="img-responsive" alt="" src="Admin/'.$value["imagen"].'">
-                            <a href="'.$value["enlace"].'" target="_blank" class="project-description">
+                            <a href="'.$value["enlace"]. '" target="_blank" class="project-description">
                                 <div class="project-text-holder">
-                                <div class="project-text-inner">
-                                    <h3>'.$value["descripcionCorta"]. '</h3>
-                                    <h3>We need '.$value["cantidad"].'</h3>
-                                    <h3>Each coast $'.$value["costo"].'</h3>
-                                </div>
+                                    <div class="project-text-inner">
+                                        <h3>' . $value["titulo"] . '</h3>
+                                        <p class="lh-sm">'.$value["descripcionCorta"]. '</p>
+                                        <h3>We need '.$value["cantidad"].'</h3>
+                                        <h3>Each coast $'.$value["costo"].'</h3>
+                                    </div>
                                 </div>
                             </a>
                         </div>';
