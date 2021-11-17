d058db6232d55eb94dbde8cd3be58a67d464dae9
Allan Mendoza <allanmaleman@gmail.com>
2021-11-17 15:08:40 -0600

    Se Trabaja en barra de progreso, aun no funciona

--- vista/modulos/index.php
+++ vista/modulos/index.php
@@ -14,12 +14,14 @@
   </div>
 </div>
 
-<div class="progress">
+<!-- NO FUNCIONA, TRABAJAR EN PROPUESTA DE BARRA DE PROGRESO -->
+<!-- <div class="progress">
     <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
     aria-valuemin="0" aria-valuemax="100" style="width:40%">
       40% Complete (success)
     </div>
-  </div>
+  </div> -->
+<!-- FIN -->
 
 <script>
     document.addEventListener("DOMContentLoaded", function (event) {
